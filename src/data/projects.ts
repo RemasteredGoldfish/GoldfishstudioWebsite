ef33153875a83be8d59466889825b6c40b0a880e
me <me@gmail.com>
2026-08-18 11:49:07 +0200

    revert to pre-two-day state and make homepage video grid vertical with autoplay

---
+++ src/data/projects.ts
@@ -0,0 +1,13 @@
+export const projects = [
+  // Vertical videos first
+  { title: 'Coach Kux', videoId: '7gZLxHgUQx8', muxId: 'PIp6EDEDue008BuRkVus5yWVZTSP9QwgxYIUAKvA26yc', vertical: true, tag: 'Reel', year: '2026' },
+  { title: 'Basketball Commercial', videoId: '7gZLxHgUQx8', muxId: '4BIw4S4zfZ401cF5cL54YY7hPAr00BoQFH6YrQpT5OclE', vertical: true, tag: 'Commercial', year: '2026' },
+  { title: 'Martabak', videoId: '9nALKayuaE0', muxId: 'LyrdTPrrRFX8rpoK727fJMZcOHgrmv00QzpeVpYRWIXw', vertical: true, tag: 'Commercial', year: '2026' },
+  { title: 'Barber', videoId: '7gZLxHgUQx8', muxId: '4hCHlswi3XjkKSrjIfY9xN2Lbyp6AzK00yhqBWpbk4zg', vertical: true, tag: 'Commercial', year: '2026' },
+  { title: 'Pathfinder Day', videoId: 'otjwjZ9A7Gc', muxId: 'JA7D3foAf02DySqLel39EZCXM9Jq00KhsRU8lHWPZ5x6U', vertical: true, tag: 'Event', year: '2026' },
+  { title: 'Office Amelia Video', videoId: '2zAnE4BHG3g', muxId: 'Wbla02Gv5901KwOncSIDQ8Afmwk8Uwlls9hma02w1ZzRnI', vertical: true, tag: 'Social Media', year: '2026' },
+  { title: 'I Am Uniek Podcast', videoId: '7gZLxHgUQx8', muxId: 'lvxpMax4DXpsO4NxhniXJo9nb00wIN95jObcIPH7BNEA', vertical: true, tag: 'Podcast', year: '2026' },
+  // Horizontal videos
+  { title: 'Gospel Celebrations Friday Delivery', videoId: 'fvcC-OiFOXM', muxId: null, vertical: false, tag: 'Concert', year: '2026' },
+  { title: 'Office Website 4K', videoId: 'oLuet3RdY1A', muxId: 'O00qf5HrTommDuoYh6xIq3GloLj1IM5WqzjtS9aBWOwg', vertical: false, tag: 'Corporate', year: '2026' },
+];
